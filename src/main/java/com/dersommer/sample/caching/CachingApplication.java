cd4dbf877833bef618f2c6521fb5feba559ddbf8
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2017-11-03 14:08:46 -0200

    Added example of @ConfigurationProperties

--- src/main/java/com/dersommer/sample/caching/CachingApplication.java
+++ src/main/java/com/dersommer/sample/caching/CachingApplication.java
@@ -1,11 +1,13 @@
 package com.dersommer.sample.caching;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAutoConfiguration
 /**
  * Spring bootstrap class
  */
--- src/main/java/com/dersommer/sample/caching/config/WebConfig.java
+++ src/main/java/com/dersommer/sample/caching/config/WebConfig.java
@@ -2,13 +2,28 @@ package com.dersommer.sample.caching.config;
 
 import com.dersommer.sample.caching.resource.QueryCountry;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class WebConfig extends ResourceConfig {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
+
+    @Autowired
+    private Lists lists;
+
     public WebConfig() {
         register(QueryCountry.class);
+    }
+
+    @PostConstruct
+    public void init() {
+        lists.getAList().forEach(item -> LOGGER.info(item));
 
     }
 }
